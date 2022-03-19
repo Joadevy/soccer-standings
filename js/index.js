216921ca2588373eac588260e33480f999dce81b
Joaquin Arlettaz <jjoaquinarlettaz@gmail.com>
2022-03-19 13:45:19 -0300

    Displaying team name+logo from the API

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ let endpoint = "https://api-football-standings.azharimm.site/leagues";
 body = document.getElementById('body');
 container = document.getElementById('info');
 
-const getLogo = async() => {
+/* const getLogo = async() => {
     try {
         let request = await fetch(endpoint);
         let response = await request.json();
@@ -12,25 +12,29 @@ const getLogo = async() => {
     } catch (error) {
         console.log(error);
     }
-}
+} */
 
 const getStandings = async(endpoint) => {
     try {
         let request = await fetch(endpoint);
         let response = await request.json();
-        let div = document.createElement('div');
+        let fragment =document.createDocumentFragment();
         for (let team in response.data['standings']) {
-            console.log(response.data['standings'][team].team.name);
-            console.log(response.data['standings'][team].team.logos[0].href); // There are the logo of each team.
+            console.log(response.data['standings'][team].team.name); // Shows the name of each team
+            console.log(response.data['standings'][team].team.logos[0].href); // Shows the logo of each team
+            let div = document.createElement('div');
+            div.classList.add('team-container');
             let logo = document.createElement('img');
             logo.src = response.data['standings'][team].team.logos[0].href;
-            document.getElementById('info').appendChild(logo);
+            logo.classList.add('team-logo');
+            div.appendChild(logo);
             let name = document.createElement('p');
             name.textContent = response.data['standings'][team].team.name;
-            document.getElementById('info').appendChild(name);
-            
+            name.classList.add('team-name');
+            div.appendChild(name);
+            fragment.appendChild(div);
         }
-        document.getElementById('info').appendChild(div);
+        document.getElementById('info').appendChild(fragment); // Rendering in the HTML.
     } catch (error) {
         console.log(error);
     }
@@ -42,7 +46,7 @@ function encodeQueryData(data){
     return result;
 }
 
-let league = 'arg.1'; // ita.1, ger.1, eng.1, fra.1, arg.1
+let league = 'fra.1'; // ita.1, ger.1, eng.1, fra.1, arg.1
 let season = '2019';
 
 let data = {
@@ -52,5 +56,5 @@ let data = {
 
 endpoint = 'https://api-football-standings.azharimm.site/leagues/';
 console.log(endpoint+encodeQueryData(data));
-getLogo();
+//getLogo();
 getStandings(endpoint+encodeQueryData(data));
